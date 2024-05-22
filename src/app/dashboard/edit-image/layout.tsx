502e60afdd8b5d91b40ab4c6f397b28316a5c05d
Orbin-Ahmed <orbin.ahmed.acanto@g.bracu.ac.bd>
2024-05-22 19:32:13 +0400

    added layout in edit page component

---
+++ src/app/dashboard/edit-image/layout.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from "next";
+import Page from "./page";
+
+export const metadata: Metadata = {
+  title: "Design Studio || Keystone Engineering Consultant",
+  description: "This is the edit image page",
+};
+export default Page;
--- src/app/dashboard/edit-image/page.tsx
+++ src/app/dashboard/edit-image/page.tsx
@@ -1,10 +1,5 @@
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-export const metadata: Metadata = {
-  title: "Design Studio || Keystone Engineering Consultant",
-  description: "This is the edit image page",
-};
 
 type Props = {};
 
@@ -13,7 +8,6 @@ const EditImage = (props: Props) => {
     <DefaultLayout>
       <div className="mx-auto max-w-242.5">
         <Breadcrumb pageName="Design Studio" />
-        <div>Design Studio</div>
       </div>
     </DefaultLayout>
   );
