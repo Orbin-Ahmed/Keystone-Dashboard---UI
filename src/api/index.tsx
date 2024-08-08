7858fb331b6048ca4a9a3889b1929b5fccdeec78
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2024-08-08 11:57:42 +0400

    Update index.tsx

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -197,34 +197,22 @@ export const pinterestImageData = async (searchTerm: string, page: number) => {
   }
 
   const url = `${API_BASE_URL}api/images/search?query=${searchTerm}&page_size=20&page_number=${page}`;
-  const retryLimit = 3;
-  let attempt = 0;
 
-  while (attempt < retryLimit) {
-    try {
-      const response = await axios({
-        method: "get",
-        url: url,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token ${token}`,
-        },
-        timeout: 300000,
-      });
+  try {
+    const response = await axios({
+      method: "get",
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${token}`,
+      },
+      timeout: 300000,
+    });
 
-      return response.data;
-    } catch (error) {
-      attempt++;
-      if (axios.isAxiosError(error) && error.code === "ECONNABORTED") {
-        console.error("Timeout error, retrying...", attempt);
-      } else {
-        console.error("Error fetching data:", error);
-        break;
-      }
-    }
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
-
-  throw new Error("Failed to fetch data after multiple attempts.");
 };
 
 export const houzzImageData = async (searchTerm: string, page: Number) => {
