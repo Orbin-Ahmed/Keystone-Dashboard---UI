4c1b1d7b51214f4992bdb6f8667498d04a6801c3
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2024-10-25 22:34:14 -0400

    Update layout.tsx

--- src/app/floor-planner/layout.tsx
+++ src/app/floor-planner/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import Page from "./page";
 
 export const metadata: Metadata = {
-  title: "Floor Planner || Keystone Engineering Consultant",
+  title: "Floor Planner APP || Keystone Engineering Consultant",
   description: "This is the floor planner",
 };
 export default Page;
