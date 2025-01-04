d73a2c5de293c4b0ea23f70ce08d6e15ff81c74a
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-01-04 17:18:58 -0500

    Update page.tsx

--- src/app/floor-planner/page.tsx
+++ src/app/floor-planner/page.tsx
@@ -787,7 +787,7 @@ const FloorPlanner = () => {
         </label>
       </div>
 
-      <div className="fixed bottom-8 right-8 z-50 flex -translate-x-1/2 transform items-center gap-4">
+      <div className="fixed bottom-8 right-4 z-50 flex transform items-center gap-4">
         <button
           className="rounded-full px-4 py-2 shadow-xl"
           onClick={() => handlePlaneChange("floor")}
