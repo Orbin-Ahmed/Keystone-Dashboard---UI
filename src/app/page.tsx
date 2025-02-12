b72deab044c4666f6d3459a301b0b3553539eba6
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-02-12 01:12:39 -0500

    Removed Client Content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import { Metadata } from "next";
-import HeroSection from "@/components/Client/HeroSection";
-import AboutComponent from "@/components/Client/AboutComponent";
-import CatalogueSection from "@/components/Client/CatalogueSection";
-import CatalogSwiperSection from "@/components/Client/CatalogSwiperCection";
-import ContactSection from "@/components/Client/ContactSection";
-import CompanySection from "@/components/Client/CompanySection";
-import Header from "@/components/Client/Header";
-import Footer from "@/components/Client/Footer";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home || Ideal Home Interior",
@@ -16,14 +9,20 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <Header />
-      <HeroSection />
-      {/* <CompanySection /> */}
-      <AboutComponent />
-      <CatalogueSection />
-      <CatalogSwiperSection />
-      <ContactSection />
-      <Footer />
+      <div className="bg-gray-100 flex min-h-screen items-center justify-center">
+        <div className="space-x-4">
+          <Link href="/dashboard">
+            <button className="rounded-xl bg-blue-500 px-6 py-3 text-white shadow-md transition hover:bg-blue-600">
+              Login
+            </button>
+          </Link>
+          <Link href="https://www.idealhomeuae.com/">
+            <button className="rounded-xl bg-green-500 px-6 py-3 text-white shadow-md transition hover:bg-green-600">
+              Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
