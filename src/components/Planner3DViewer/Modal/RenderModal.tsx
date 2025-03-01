7585870d84740e50fc788e89c790bad40153b269
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-03-01 15:02:13 -0500

    Update RenderModal.tsx

--- src/components/Planner3DViewer/Modal/RenderModal.tsx
+++ src/components/Planner3DViewer/Modal/RenderModal.tsx
@@ -350,24 +350,6 @@ const RenderModal: React.FC<RenderModalProps> = ({
     setTheme("day");
   };
 
-  // useEffect(() => {
-  //   const handleKeyDown = (e: KeyboardEvent) => {
-  //     console.log("Key pressed:", e.key);
-  //     console.log("Default prevented:", e.defaultPrevented);
-  //     console.log("Event object:", e);
-
-  //     if (e.key === "Backspace") {
-  //       console.log("Backspace pressed");
-  //     }
-  //   };
-
-  //   window.addEventListener("keydown", handleKeyDown);
-
-  //   return () => {
-  //     window.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
-
   if (!isOpen) return null;
 
   return (
