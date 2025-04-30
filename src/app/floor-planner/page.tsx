fbafc896c6e7c2c948df04cbf52aeea6b4394db4
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-04-30 01:55:00 -0400

    Update page.tsx

--- src/app/floor-planner/page.tsx
+++ src/app/floor-planner/page.tsx
@@ -964,7 +964,7 @@ const FloorPlanner = () => {
         </label>
       </div>
 
-      {viewMode === "2D" && (
+      {viewMode === "2D" && !isSidebarVisible && (
         <div className="fixed bottom-8 right-4 z-50 flex transform items-center gap-4">
           <button
             className="rounded-full px-4 py-2 shadow-xl"
