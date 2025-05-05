2aa7fbfadd04477d16576baaa7bf86d458d6ad89
Orbin-Ahmed <93181928+Orbin-Ahmed@users.noreply.github.com>
2025-05-05 02:10:44 -0400

    Update CustomizeItemModal.tsx

--- src/components/Planner3DViewer/Modal/CustomizeItemModal.tsx
+++ src/components/Planner3DViewer/Modal/CustomizeItemModal.tsx
@@ -107,7 +107,6 @@ const CustomizeItemModal: React.FC<CustomizeItemModalProps> = ({
     const newCustomizations = { ...customizations };
     selectedGroups.forEach((group) => {
       newCustomizations[group.groupName] = {
-        ...newCustomizations[group.groupName],
         color: localColor !== "#ffffff" ? localColor : undefined,
         brightness: localBrightness,
         opacity: localOpacity !== 100 ? localOpacity : undefined,
@@ -140,7 +139,6 @@ const CustomizeItemModal: React.FC<CustomizeItemModalProps> = ({
     const newCustomizations = { ...customizations };
     selectedGroups.forEach((group) => {
       newCustomizations[group.groupName] = {
-        // ...newCustomizations[group.groupName],
         emissionColor:
           localEmissionColor !== "#000000" ? localEmissionColor : undefined,
         emissionStrength:
